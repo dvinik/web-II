6bc074213e4e108bc1eebb81943628cf0cff2cba
Ratana <nanadoll100@gmail.com>
2018-01-24 20:17:07 +0700

    fix database connection

--- ratana/config.php
+++ ratana/config.php
@@ -2,8 +2,8 @@
 
 
 // connect database
-// $link = mysqli_connect("localhost", "root", "root", 'accounting'); // local
-$link = mysqli_connect("58.8.255.199", "Ratana", "DDp6ZN9j9KOm7esG", 'accounting'); // server
+// $link = mysqli_connect("localhost", "root", "root", 'accounting1'); // local
+$link = mysqli_connect("vinix.homeftp.net", "Ratana", "DDp6ZN9j9KOm7esG", 'accounting'); // server
 
 if (!$link) {
     die('Could not connect: ' . mysqli_error(link));
